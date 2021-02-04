2bbc7d3cf619bc5d2405ba20b0967e180044c00a
pdjp <kknntt0522@gmail.com>
2021-02-05 02:16:02 +0900

    master

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -9,8 +9,12 @@ class SchedulesController < ApplicationController
   end
   def create
     @schedule = Schedule.new(content: params[:content])
-    @schedule.save 
-    redirect_to("/index")
+    if @schedule.save 
+      flash[:notice] = "作成しました"
+      redirect_to("/index")    
+    else
+      render("schedules/new")
+    end  
   end  
   def edit
     @schedule = Schedule.find_by(id: params[:id])
@@ -18,12 +22,17 @@ class SchedulesController < ApplicationController
   def update
     @schedule = Schedule.find_by(id: params[:id])
     @schedule.content = params[:content]
-    @schedule.save
-    redirect_to("/index")
+    if @schedule.save
+      flash[:notice] = "編集しました"
+      redirect_to("/index")
+    else
+      render("schedules/edit")
+    end  
   end
   def destroy
     @schedule = Schedule.find_by(id: params[:id])
     @schedule.destroy
+    flash[:notice] = "削除しました"
     redirect_to("/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,12 @@ Rails.application.routes.draw do
   post 'schedules/:id/update' => 'schedules#update'
   post "schedules/:id/destroy" => "schedules#destroy"
 
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get 'users/index'
+  get "users/:id" => "users#show"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
