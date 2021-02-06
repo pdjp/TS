c4dd0f07e139a588a237eed763c4d46f36512080
pdjp <kknntt0522@gmail.com>
2021-02-06 20:25:37 +0900

    master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
   def index
     @users = User.all
   end
@@ -9,8 +12,9 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(name: params[:name], email: params[:email],password: params[:password])
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -36,12 +40,27 @@ class UsersController < ApplicationController
   end
   def login
     @user = User.find_by(email: params[:email], password: params[:password])
-    # @userが存在するかどうかを判定するif文を作成してください
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/index")
     else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      
+      @email = params[:email]
+      @password = params[:password]
       render("users/login_form")
+    end
+  end
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/index")
     end
   end
 end
