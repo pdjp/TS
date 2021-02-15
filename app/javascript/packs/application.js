410cf0f1a62e101b256a12ca098dbee9a3426c64
pdjp <kknntt0522@gmail.com>
2021-02-16 00:19:16 +0900

    master

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -11,3 +12,6 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+$(function(){
+  setTimeout("$('.flash).fadeOut('slow')", 2000);
+});
